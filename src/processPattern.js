6c0ad0906d6802ce6d97066643a4e88b150bc8e9
kevlened <boyettel@gmail.com>
2016-10-23 11:07:17 -0700

    Added webpack-dev-server tests

--- src/processPattern.js
+++ src/processPattern.js
@@ -71,7 +71,7 @@ export default function processPattern(globalRef, pattern) {
 
         if (path.isAbsolute(file.webpackTo)) {
             if (output === '/') {
-                throw '[copy-webpack-plugin] Using older versions of webpack-dev-server, devServer.outputPath must be defined to copy absolute files';
+                throw '[copy-webpack-plugin] Using older versions of webpack-dev-server, devServer.outputPath must be defined to write to absolute paths';
             }
 
             file.webpackTo = path.relative(output, file.webpackTo);
--- tests/index.js
+++ tests/index.js
@@ -16,13 +16,17 @@ const HELPER_DIR = path.join(__dirname, 'helpers');
 const TEMP_DIR = path.join(__dirname, 'tempdir');
 
 class MockCompiler {
-    constructor () {
+    constructor (options = {}) {
         this.options = {
             context: HELPER_DIR,
             output: {
-                path: BUILD_DIR
+                path: options.outputPath || BUILD_DIR
             }
         };
+
+        if (options.devServer && options.devServer.outputPath) {
+            _.set(this.options, 'devServer.outputPath', options.devServer.outputPath);
+        }
 
         this.outputFileSystem = {
             constructor: {
@@ -434,6 +438,45 @@ describe('apply function', () => {
             runEmit({
                 expectedAssetKeys: [
                     'file.txt'
+                ],
+                patterns: [{
+                    from: 'file.txt',
+                    to: BUILD_DIR
+                }]
+            })
+            .then(done)
+            .catch(done);
+        });
+
+        it('allows absolute to if outpath is defined with webpack-dev-server', (done) => {
+            runEmit({
+                compiler: new MockCompiler({
+                    outputPath: '/',
+                    devServer: {
+                        outputPath: BUILD_DIR
+                    }
+                }),
+                expectedAssetKeys: [
+                    'file.txt'
+                ],
+                patterns: [{
+                    from: 'file.txt',
+                    to: BUILD_DIR
+                }]
+            })
+            .then(done)
+            .catch(done);
+        });
+
+        it('throws an error when output path isn\'t defined with webpack-dev-server', (done) => {
+            runEmit({
+                compiler: new MockCompiler({
+                    outputPath: '/'
+                }),
+                expectedAssetKeys: [],
+                expectedErrors: [
+                    '[copy-webpack-plugin] Using older versions of webpack-dev-server, devServer.outputPath must be ' +
+                    'defined to write to absolute paths'
                 ],
                 patterns: [{
                     from: 'file.txt',
