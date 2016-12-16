0d5484e129e971ffe100b26170d6b514469c81d6
Miroslav Jonas <meeroslav@yahoo.com>
2016-12-16 14:09:04 +0100

    Fix OS sensitive test

--- tests/index.js
+++ tests/index.js
@@ -479,7 +479,7 @@ describe('apply function', () => {
                     'merged.txt'
                 ],
                 expectedAssetContent: {
-                    'merged.txt': 'new, directory:new'
+                    'merged.txt': './new, directory/new'
                 },
                 patterns: [{
                     from: '**/*.txt',
@@ -488,13 +488,8 @@ describe('apply function', () => {
                         if (!content.length) {
                             return content;
                         }
-                        let segments = relativePath.split('\\');
-                        segments.pop();
-                        while(segments.length) {
-                            let segment = segments.pop();
-                            content = `${segment}:${content}`;
-                        }
-                        return content;
+                        let segment = path.dirname(relativePath);
+                        return `${segment}/${content}`;
                     },
                     merge: function(content1, content2) {
                         if (!content1.length) {
