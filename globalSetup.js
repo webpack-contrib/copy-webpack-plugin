325af03b45e62924857b45763ccfb7966d2be2c6
evilebottnawi <sheo13666q@gmail.com>
2019-07-25 16:48:12 +0300

    test: refactor

---
+++ globalSetup.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mkdirp = require('mkdirp');
+
+const removeIllegalCharacterForWindows = require('./test/utils/removeIllegalCharacterForWindows');
+
+const baseDir = path.resolve(__dirname, 'test/helpers');
+
+const specialFiles = {
+  '[special?directory]/nested/nestedfile.txt': '',
+  '[special?directory]/(special-*file).txt': 'special',
+  '[special?directory]/directoryfile.txt': 'new',
+};
+
+module.exports = () => {
+  Object.keys(specialFiles).forEach((originFile) => {
+    const file = removeIllegalCharacterForWindows(originFile);
+    const dir = path.dirname(file);
+
+    mkdirp.sync(path.join(baseDir, dir));
+
+    fs.writeFileSync(path.join(baseDir, file), specialFiles[originFile]);
+  });
+};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  globalSetup: '<rootDir>/globalSetup.js',
+};
