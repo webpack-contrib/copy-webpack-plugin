9035431f02f7b965ae4d672136c654b852763469
kewm <ke_weiming@sina.com>
2019-12-21 23:58:34 +0800

    fix(): remove p-limit from the processPattern.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,17 +2,6 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
-### [5.1.2](https://github.com/webpack-contrib/copy-webpack-plugin/compare/v5.1.1...v5.1.2) (2019-12-17)
-
-
-### Features
-
-* add concurrency control
-
-### Bug Fixes
-
-* fix error: too many open files
-
 ### [5.1.1](https://github.com/webpack-contrib/copy-webpack-plugin/compare/v5.1.0...v5.1.1) (2019-12-12)
 
 
--- src/processPattern.js
+++ src/processPattern.js
@@ -1,13 +1,12 @@
 import path from 'path';
 
 import globby from 'globby';
-import pLimit from 'p-limit';
 import minimatch from 'minimatch';
 
 import isObject from './utils/isObject';
 
 export default function processPattern(globalRef, pattern) {
-  const { logger, output, concurrency, compilation } = globalRef;
+  const { logger, output, compilation } = globalRef;
   const globOptions = Object.assign(
     {
       cwd: pattern.context,
@@ -22,100 +21,96 @@ export default function processPattern(globalRef, pattern) {
     return Promise.resolve();
   }
 
-  const limit = pLimit(concurrency || 100);
-
   logger.info(
     `begin globbing '${pattern.glob}' with a context of '${pattern.context}'`
   );
 
   return globby(pattern.glob, globOptions).then((paths) =>
     Promise.all(
-      paths.map((from) =>
-        limit(() => {
-          const file = {
-            force: pattern.force,
-            absoluteFrom: path.resolve(pattern.context, from),
+      paths.map((from) => {
+        const file = {
+          force: pattern.force,
+          absoluteFrom: path.resolve(pattern.context, from),
+        };
+
+        file.relativeFrom = path.relative(pattern.context, file.absoluteFrom);
+
+        if (pattern.flatten) {
+          file.relativeFrom = path.basename(file.relativeFrom);
+        }
+
+        logger.debug(`found ${from}`);
+
+        // Check the ignore list
+        let il = pattern.ignore.length;
+
+        // eslint-disable-next-line no-plusplus
+        while (il--) {
+          const ignoreGlob = pattern.ignore[il];
+
+          let globParams = {
+            dot: true,
+            matchBase: true,
           };
 
-          file.relativeFrom = path.relative(pattern.context, file.absoluteFrom);
+          let glob;
 
-          if (pattern.flatten) {
-            file.relativeFrom = path.basename(file.relativeFrom);
+          if (typeof ignoreGlob === 'string') {
+            glob = ignoreGlob;
+          } else if (isObject(ignoreGlob)) {
+            glob = ignoreGlob.glob || '';
+
+            const ignoreGlobParams = Object.assign({}, ignoreGlob);
+            delete ignoreGlobParams.glob;
+
+            // Overwrite minimatch defaults
+            globParams = Object.assign(globParams, ignoreGlobParams);
+          } else {
+            glob = '';
           }
 
-          logger.debug(`found ${from}`);
+          logger.debug(`testing ${glob} against ${file.relativeFrom}`);
 
-          // Check the ignore list
-          let il = pattern.ignore.length;
+          if (minimatch(file.relativeFrom, glob, globParams)) {
+            logger.info(
+              `ignoring '${file.relativeFrom}', because it matches the ignore glob '${glob}'`
+            );
 
-          // eslint-disable-next-line no-plusplus
-          while (il--) {
-            const ignoreGlob = pattern.ignore[il];
-
-            let globParams = {
-              dot: true,
-              matchBase: true,
-            };
-
-            let glob;
-
-            if (typeof ignoreGlob === 'string') {
-              glob = ignoreGlob;
-            } else if (isObject(ignoreGlob)) {
-              glob = ignoreGlob.glob || '';
-
-              const ignoreGlobParams = Object.assign({}, ignoreGlob);
-              delete ignoreGlobParams.glob;
-
-              // Overwrite minimatch defaults
-              globParams = Object.assign(globParams, ignoreGlobParams);
-            } else {
-              glob = '';
-            }
-
-            logger.debug(`testing ${glob} against ${file.relativeFrom}`);
-
-            if (minimatch(file.relativeFrom, glob, globParams)) {
-              logger.info(
-                `ignoring '${file.relativeFrom}', because it matches the ignore glob '${glob}'`
-              );
-
-              return Promise.resolve();
-            }
-
-            logger.debug(`${glob} doesn't match ${file.relativeFrom}`);
+            return null;
           }
 
-          // Change the to path to be relative for webpack
-          if (pattern.toType === 'dir') {
-            file.webpackTo = path.join(pattern.to, file.relativeFrom);
-          } else if (pattern.toType === 'file') {
-            file.webpackTo = pattern.to || file.relativeFrom;
-          } else if (pattern.toType === 'template') {
-            file.webpackTo = pattern.to;
-            file.webpackToRegExp = pattern.test;
+          logger.debug(`${glob} doesn't match ${file.relativeFrom}`);
+        }
+
+        // Change the to path to be relative for webpack
+        if (pattern.toType === 'dir') {
+          file.webpackTo = path.join(pattern.to, file.relativeFrom);
+        } else if (pattern.toType === 'file') {
+          file.webpackTo = pattern.to || file.relativeFrom;
+        } else if (pattern.toType === 'template') {
+          file.webpackTo = pattern.to;
+          file.webpackToRegExp = pattern.test;
+        }
+
+        if (path.isAbsolute(file.webpackTo)) {
+          if (output === '/') {
+            const message =
+              'using older versions of webpack-dev-server, devServer.outputPath must be defined to write to absolute paths';
+
+            logger.error(message);
+
+            compilation.errors.push(new Error(message));
           }
 
-          if (path.isAbsolute(file.webpackTo)) {
-            if (output === '/') {
-              const message =
-                'using older versions of webpack-dev-server, devServer.outputPath must be defined to write to absolute paths';
+          file.webpackTo = path.relative(output, file.webpackTo);
+        }
 
-              logger.error(message);
+        logger.info(
+          `determined that '${from}' should write to '${file.webpackTo}'`
+        );
 
-              compilation.errors.push(new Error(message));
-            }
-
-            file.webpackTo = path.relative(output, file.webpackTo);
-          }
-
-          logger.info(
-            `determined that '${from}' should write to '${file.webpackTo}'`
-          );
-
-          return file;
-        })
-      )
+        return file;
+      })
     )
   );
 }
