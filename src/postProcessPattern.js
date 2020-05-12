f89b2ab03e8ac722644462c49df4614230af2bd6
cap-Bernardito <cap-Bernardito@yandex.ru>
2020-05-12 14:51:32 +0300

    refactor: refactor

--- src/postProcessPattern.js
+++ src/postProcessPattern.js
@@ -14,23 +14,22 @@ import { stat, readFile } from './utils/promisify';
 
 /* eslint-disable no-param-reassign */
 
-export default function postProcessPattern(globalRef, pattern, file) {
+export default async function postProcessPattern(globalRef, pattern, file) {
   const { logger, compilation, inputFileSystem } = globalRef;
 
   logger.debug(`getting stats for '${file.absoluteFrom}' to write to assets`);
 
-  const getStats = pattern.stats
-    ? Promise.resolve().then(() => pattern.stats)
-    : stat(inputFileSystem, file.absoluteFrom);
+  try {
+    const getStats = pattern.stats
+      ? Promise.resolve().then(() => pattern.stats)
+      : stat(inputFileSystem, file.absoluteFrom);
 
-  return getStats.then((stats) => {
-    // We don't write empty directories
+    const stats = await getStats;
+
     if (stats.isDirectory()) {
       logger.debug(
         `skipping '${file.absoluteFrom}' because it is empty directory`
       );
-
-      return Promise.resolve();
     }
 
     // If this came from a glob, add it to the file watchlist
@@ -41,127 +40,109 @@ export default function postProcessPattern(globalRef, pattern, file) {
 
     logger.debug(`reading '${file.absoluteFrom}' to write to assets`);
 
-    return readFile(inputFileSystem, file.absoluteFrom)
-      .then((content) => {
-        if (pattern.transform) {
-          logger.log(`transforming content for '${file.absoluteFrom}'`);
+    let content = await readFile(inputFileSystem, file.absoluteFrom);
 
-          // eslint-disable-next-line no-shadow
-          const transform = (content, absoluteFrom) =>
-            pattern.transform(content, absoluteFrom);
+    if (pattern.transform) {
+      logger.log(`transforming content for '${file.absoluteFrom}'`);
 
-          if (pattern.cacheTransform) {
-            if (!globalRef.cacheDir) {
-              globalRef.cacheDir =
-                findCacheDir({ name: 'copy-webpack-plugin' }) || os.tmpdir();
-            }
+      // eslint-disable-next-line no-shadow
+      const transform = (content, absoluteFrom) =>
+        pattern.transform(content, absoluteFrom);
 
-            const cacheKey = pattern.cacheTransform.key
-              ? pattern.cacheTransform.key
-              : serialize({
-                  name,
-                  version,
-                  pattern,
-                  hash: crypto.createHash('md4').update(content).digest('hex'),
-                });
-
-            return cacache.get(globalRef.cacheDir, cacheKey).then(
-              (result) => {
-                logger.debug(
-                  `getting cached transformation for '${file.absoluteFrom}'`
-                );
-
-                return result.data;
-              },
-              () =>
-                Promise.resolve()
-                  .then(() => transform(content, file.absoluteFrom))
-                  // eslint-disable-next-line no-shadow
-                  .then((content) => {
-                    logger.debug(
-                      `caching transformation for '${file.absoluteFrom}'`
-                    );
-
-                    return cacache
-                      .put(globalRef.cacheDir, cacheKey, content)
-                      .then(() => content);
-                  })
-            );
-          }
-
-          content = transform(content, file.absoluteFrom);
+      if (pattern.cacheTransform) {
+        if (!globalRef.cacheDir) {
+          globalRef.cacheDir =
+            findCacheDir({ name: 'copy-webpack-plugin' }) || os.tmpdir();
         }
 
-        return content;
-      })
-      .then((content) => {
-        if (pattern.toType === 'template') {
-          logger.log(
-            `interpolating template '${file.webpackTo}' for '${file.relativeFrom}'`
-          );
-
-          // If it doesn't have an extension, remove it from the pattern
-          // ie. [name].[ext] or [name][ext] both become [name]
-          if (!path.extname(file.relativeFrom)) {
-            file.webpackTo = file.webpackTo.replace(/\.?\[ext\]/g, '');
-          }
-
-          file.webpackTo = loaderUtils.interpolateName(
-            { resourcePath: file.absoluteFrom },
-            file.webpackTo,
-            {
-              content,
-              regExp: file.webpackToRegExp,
-              context: pattern.context,
-            }
-          );
-
-          // Bug in `loader-utils`, package convert `\\` to `/`, need fix in loader-utils
-          file.webpackTo = path.normalize(file.webpackTo);
-        }
-
-        return content;
-      })
-      .then((content) => {
-        if (pattern.transformPath) {
-          logger.log(
-            `transforming path '${file.webpackTo}' for '${file.absoluteFrom}'`
-          );
-
-          return Promise.resolve()
-            .then(() =>
-              pattern.transformPath(file.webpackTo, file.absoluteFrom)
-            )
-            .then((newPath) => {
-              file.webpackTo = newPath;
-
-              return content;
+        const cacheKey = pattern.cacheTransform.key
+          ? pattern.cacheTransform.key
+          : serialize({
+              name,
+              version,
+              pattern,
+              hash: crypto.createHash('md4').update(content).digest('hex'),
             });
-        }
 
+        try {
+          const result = await cacache.get(globalRef.cacheDir, cacheKey);
+
+          logger.debug(
+            `getting cached transformation for '${file.absoluteFrom}'`
+          );
+
+          content = await result.data;
+        } catch (e) {
+          content = await transform(content, file.absoluteFrom);
+
+          logger.debug(`caching transformation for '${file.absoluteFrom}'`);
+
+          content = await cacache
+            .put(globalRef.cacheDir, cacheKey, content)
+            .then(() => content);
+        }
+      } else {
+        content = await transform(content, file.absoluteFrom);
+      }
+    }
+
+    if (pattern.toType === 'template') {
+      logger.log(
+        `interpolating template '${file.webpackTo}' for '${file.relativeFrom}'`
+      );
+
+      // If it doesn't have an extension, remove it from the pattern
+      // ie. [name].[ext] or [name][ext] both become [name]
+      if (!path.extname(file.relativeFrom)) {
+        file.webpackTo = file.webpackTo.replace(/\.?\[ext\]/g, '');
+      }
+
+      file.webpackTo = loaderUtils.interpolateName(
+        { resourcePath: file.absoluteFrom },
+        file.webpackTo,
+        {
+          content,
+          regExp: file.webpackToRegExp,
+          context: pattern.context,
+        }
+      );
+
+      // Bug in `loader-utils`, package convert `\\` to `/`, need fix in loader-utils
+      file.webpackTo = path.normalize(file.webpackTo);
+    }
+
+    if (pattern.transformPath) {
+      logger.log(
+        `transforming path '${file.webpackTo}' for '${file.absoluteFrom}'`
+      );
+
+      file.webpackTo = await pattern.transformPath(
+        file.webpackTo,
+        file.absoluteFrom
+      );
+    }
+
+    const targetPath = normalizePath(file.webpackTo);
+
+    if (compilation.assets[targetPath] && !file.force) {
+      logger.log(`skipping '${file.webpackTo}', because it already exists`);
+
+      return;
+    }
+
+    logger.log(
+      `writing '${file.webpackTo}' to compilation assets from '${file.absoluteFrom}'`
+    );
+
+    compilation.assets[targetPath] = {
+      size() {
+        return stats.size;
+      },
+      source() {
         return content;
-      })
-      .then((content) => {
-        const targetPath = normalizePath(file.webpackTo);
-
-        if (compilation.assets[targetPath] && !file.force) {
-          logger.log(`skipping '${file.webpackTo}', because it already exists`);
-
-          return;
-        }
-
-        logger.log(
-          `writing '${file.webpackTo}' to compilation assets from '${file.absoluteFrom}'`
-        );
-
-        compilation.assets[targetPath] = {
-          size() {
-            return stats.size;
-          },
-          source() {
-            return content;
-          },
-        };
-      });
-  });
+      },
+    };
+  } catch (error) {
+    throw error;
+  }
 }
