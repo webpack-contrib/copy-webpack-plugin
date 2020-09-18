a2848135f89638067e20a580911679fbb3b2e29d
evilebottnawi <sheo13666q@gmail.com>
2020-09-18 22:21:19 +0300

    refactor: code

--- src/index.js
+++ src/index.js
@@ -26,6 +26,86 @@ class CopyPlugin {
     this.options = options.options || {};
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  async runPattern(globalRef, inputPattern) {
+    const { logger } = globalRef;
+    const pattern =
+      typeof inputPattern === 'string'
+        ? { from: inputPattern }
+        : { ...inputPattern };
+
+    pattern.fromOrigin = pattern.from;
+    pattern.from = path.normalize(pattern.from);
+    pattern.to = path.normalize(
+      typeof pattern.to !== 'undefined' ? pattern.to : ''
+    );
+
+    pattern.context = path.normalize(
+      typeof pattern.context !== 'undefined'
+        ? !path.isAbsolute(pattern.context)
+          ? path.join(globalRef.context, pattern.context)
+          : pattern.context
+        : globalRef.context
+    );
+
+    logger.debug(`processing from: '${pattern.from}' to: '${pattern.to}'`);
+
+    const isToDirectory =
+      path.extname(pattern.to) === '' || pattern.to.slice(-1) === path.sep;
+
+    switch (true) {
+      // if toType already exists
+      case !!pattern.toType:
+        break;
+      case isTemplateLike(pattern.to):
+        pattern.toType = 'template';
+        break;
+      case isToDirectory:
+        pattern.toType = 'dir';
+        break;
+      default:
+        pattern.toType = 'file';
+    }
+
+    if (path.isAbsolute(pattern.from)) {
+      pattern.absoluteFrom = pattern.from;
+    } else {
+      pattern.absoluteFrom = path.resolve(pattern.context, pattern.from);
+    }
+
+    logger.debug(
+      `getting stats for '${pattern.absoluteFrom}' to determinate 'fromType'`
+    );
+
+    let stats;
+
+    try {
+      stats = await stat(globalRef.inputFileSystem, pattern.absoluteFrom);
+    } catch (error) {
+      // Nothing
+    }
+
+    if (stats) {
+      if (stats.isDirectory()) {
+        pattern.fromType = 'dir';
+      } else if (stats.isFile()) {
+        pattern.fromType = 'file';
+      }
+    }
+
+    const files = await processPattern(globalRef, pattern);
+
+    if (!files) {
+      return Promise.resolve();
+    }
+
+    return Promise.all(
+      files
+        .filter(Boolean)
+        .map((file) => postProcessPattern(globalRef, pattern, file))
+    );
+  }
+
   apply(compiler) {
     const pluginName = this.constructor.name;
     const limit = pLimit(this.options.concurrency || 100);
@@ -51,92 +131,7 @@ class CopyPlugin {
           try {
             assets = await Promise.all(
               this.patterns.map((item) =>
-                limit(async () => {
-                  const pattern =
-                    typeof item === 'string' ? { from: item } : { ...item };
-
-                  pattern.fromOrigin = pattern.from;
-                  pattern.from = path.normalize(pattern.from);
-                  pattern.to = path.normalize(
-                    typeof pattern.to !== 'undefined' ? pattern.to : ''
-                  );
-
-                  pattern.context = path.normalize(
-                    typeof pattern.context !== 'undefined'
-                      ? !path.isAbsolute(pattern.context)
-                        ? path.join(compiler.options.context, pattern.context)
-                        : pattern.context
-                      : compiler.options.context
-                  );
-
-                  logger.debug(
-                    `processing from: '${pattern.from}' to: '${pattern.to}'`
-                  );
-
-                  const isToDirectory =
-                    path.extname(pattern.to) === '' ||
-                    pattern.to.slice(-1) === path.sep;
-
-                  switch (true) {
-                    // if toType already exists
-                    case !!pattern.toType:
-                      break;
-                    case isTemplateLike(pattern.to):
-                      pattern.toType = 'template';
-                      break;
-                    case isToDirectory:
-                      pattern.toType = 'dir';
-                      break;
-                    default:
-                      pattern.toType = 'file';
-                  }
-
-                  if (path.isAbsolute(pattern.from)) {
-                    pattern.absoluteFrom = pattern.from;
-                  } else {
-                    pattern.absoluteFrom = path.resolve(
-                      pattern.context,
-                      pattern.from
-                    );
-                  }
-
-                  logger.debug(
-                    `getting stats for '${pattern.absoluteFrom}' to determinate 'fromType'`
-                  );
-
-                  let stats;
-
-                  try {
-                    stats = await stat(
-                      compiler.inputFileSystem,
-                      pattern.absoluteFrom
-                    );
-                  } catch (error) {
-                    // Nothing
-                  }
-
-                  if (stats) {
-                    if (stats.isDirectory()) {
-                      pattern.fromType = 'dir';
-                    } else if (stats.isFile()) {
-                      pattern.fromType = 'file';
-                    }
-                  }
-
-                  const files = await processPattern(globalRef, pattern);
-
-                  if (!files) {
-                    return Promise.resolve();
-                  }
-
-                  return Promise.all(
-                    files
-                      .filter(Boolean)
-                      .map((file) =>
-                        postProcessPattern(globalRef, pattern, file)
-                      )
-                  );
-                })
+                limit(async () => this.runPattern(globalRef, item))
               )
             );
           } catch (error) {
