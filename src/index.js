dde71f01417b9291c7029a3876e043d76beb9e8d
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2021-03-22 18:25:41 +0300

    feat: added the `transformAll` option (#596)

--- src/index.js
+++ src/index.js
@@ -69,6 +69,27 @@ class CopyPlugin {
     });
   }
 
+  static getContentHash(compiler, compilation, source) {
+    const { outputOptions } = compilation;
+    const {
+      hashDigest,
+      hashDigestLength,
+      hashFunction,
+      hashSalt,
+    } = outputOptions;
+    const hash = compiler.webpack.util.createHash(hashFunction);
+
+    if (hashSalt) {
+      hash.update(hashSalt);
+    }
+
+    hash.update(source);
+
+    const fullContentHash = hash.digest(hashDigest);
+
+    return fullContentHash.slice(0, hashDigestLength);
+  }
+
   static async runPattern(
     compiler,
     compilation,
@@ -456,7 +477,7 @@ class CopyPlugin {
             if (transform.transformer) {
               logger.log(`transforming content for '${absoluteFilename}'...`);
 
-              const buffer = result.source.source();
+              const buffer = result.source.buffer();
 
               if (transform.cache) {
                 const defaultCacheKeys = {
@@ -526,23 +547,11 @@ class CopyPlugin {
               `interpolating template '${filename}' for '${sourceFilename}'...`
             );
 
-            const { outputOptions } = compilation;
-            const {
-              hashDigest,
-              hashDigestLength,
-              hashFunction,
-              hashSalt,
-            } = outputOptions;
-            const hash = compiler.webpack.util.createHash(hashFunction);
-
-            if (hashSalt) {
-              hash.update(hashSalt);
-            }
-
-            hash.update(result.source.source());
-
-            const fullContentHash = hash.digest(hashDigest);
-            const contentHash = fullContentHash.slice(0, hashDigestLength);
+            const contentHash = CopyPlugin.getContentHash(
+              compiler,
+              compilation,
+              result.source.buffer()
+            );
             const ext = path.extname(result.sourceFilename);
             const base = path.basename(result.sourceFilename);
             const name = base.slice(0, base.length - ext.length);
@@ -634,6 +643,109 @@ class CopyPlugin {
                 }
 
                 if (assets && assets.length > 0) {
+                  if (item.transformAll) {
+                    if (typeof item.to === "undefined") {
+                      compilation.errors.push(
+                        new Error(
+                          `Invalid "pattern.to" for the "pattern.from": "${item.from}" and "pattern.transformAll" function. The "to" option must be specified.`
+                        )
+                      );
+
+                      return;
+                    }
+
+                    assets.sort((a, b) =>
+                      a.absoluteFilename > b.absoluteFilename
+                        ? 1
+                        : a.absoluteFilename < b.absoluteFilename
+                        ? -1
+                        : 0
+                    );
+
+                    const mergedEtag =
+                      assets.length === 1
+                        ? cache.getLazyHashedEtag(assets[0].source.buffer())
+                        : assets.reduce((accumulator, asset, i) => {
+                            // eslint-disable-next-line no-param-reassign
+                            accumulator = cache.mergeEtags(
+                              i === 1
+                                ? cache.getLazyHashedEtag(
+                                    accumulator.source.buffer()
+                                  )
+                                : accumulator,
+                              cache.getLazyHashedEtag(asset.source.buffer())
+                            );
+
+                            return accumulator;
+                          });
+
+                    const cacheKeys = `transformAll|${serialize({
+                      version,
+                      from: item.from,
+                      to: item.to,
+                      transformAll: item.transformAll,
+                    })}`;
+                    const eTag = cache.getLazyHashedEtag(mergedEtag);
+                    const cacheItem = cache.getItemCache(cacheKeys, eTag);
+                    let transformedAsset = await cacheItem.getPromise();
+
+                    if (!transformedAsset) {
+                      transformedAsset = { filename: item.to };
+
+                      try {
+                        transformedAsset.data = await item.transformAll(
+                          assets.map((asset) => {
+                            return {
+                              data: asset.source.buffer(),
+                              sourceFilename: asset.sourceFilename,
+                              absoluteFilename: asset.absoluteFilename,
+                            };
+                          })
+                        );
+                      } catch (error) {
+                        compilation.errors.push(error);
+
+                        return;
+                      }
+
+                      if (template.test(item.to)) {
+                        const contentHash = CopyPlugin.getContentHash(
+                          compiler,
+                          compilation,
+                          transformedAsset.data
+                        );
+
+                        const {
+                          path: interpolatedFilename,
+                          info: assetInfo,
+                        } = compilation.getPathWithInfo(
+                          normalizePath(item.to),
+                          {
+                            contentHash,
+                            chunk: {
+                              hash: contentHash,
+                              contentHash,
+                            },
+                          }
+                        );
+
+                        transformedAsset.filename = interpolatedFilename;
+                        transformedAsset.info = assetInfo;
+                      }
+
+                      const { RawSource } = compiler.webpack.sources;
+
+                      transformedAsset.source = new RawSource(
+                        transformedAsset.data
+                      );
+                      transformedAsset.force = item.force;
+
+                      await cacheItem.storePromise(transformedAsset);
+                    }
+
+                    assets = [transformedAsset];
+                  }
+
                   const priority = item.priority || 0;
 
                   if (!assetMap.has(priority)) {
