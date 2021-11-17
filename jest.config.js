5d9735d1a2e47dc0991c271e2f113cafbea38544
evilebottnawi <sheo13666q@gmail.com>
2021-11-17 17:25:02 +0300

    test: fix

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   testEnvironment: "node",
   moduleNameMapper: {
     "^globby$": "<rootDir>/test/bundled/globby/index.js",
+    "^node:fs$": "<rootDir>/test/helpers/built-in-modules/fs.js",
+    "^node:util$": "<rootDir>/test/helpers/built-in-modules/util.js",
+    "^node:path$": "<rootDir>/test/helpers/built-in-modules/path.js",
+    "^node:stream$": "<rootDir>/test/helpers/built-in-modules/stream.js",
   },
   globalSetup: "<rootDir>/globalSetup.js",
 };
